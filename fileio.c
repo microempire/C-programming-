92a2e36fa1eb53746fb9416db48a2145c54fa48a
Than Zaw Toe <microempire37@gmail.com>
2022-09-30 22:39:31 +0630

    file input data to the program and print file

--- fileio.c
+++ fileio.c
@@ -5,12 +5,12 @@ void area();
 
 int main(){
 
-    for (int i = 0 ; i < 5 ; i++){
+   
     
     area();
     system ("pause");
     
-    }
+    
 
     return 0; //
 }
@@ -18,13 +18,13 @@ int main(){
 
 void area(){
 
-    FILE* fout1 = NULL ;
+    FILE* fin = NULL ;
     FILE* fout2 = NULL ;
 
-    fout1 = fopen("r.txt" , "a");  
-    fout2 = fopen("a.txt" , "a");   // a = append , w = overwrite , r = read , r+ =read&wirte, w+,a+
+    fin = fopen("r.txt" , "r");  
+    fout2 = fopen("a2.txt" , "w");   // a = append , w = overwrite , r = read , r+ =read&wirte, w+,a+
 
-    if ((fout1 == NULL) || (fout2 == NULL))     // "&& " = and operator , "||" = or operator
+    if ((fin == NULL) || (fout2 == NULL))     // "&& " = and operator , "||" = or operator
     {
         printf("r.txt cannot open");
     
@@ -36,17 +36,24 @@ void area(){
     float r = 0.0;
     float a = 0.0;
 
-    printf("Please entre r : ");
-    scanf("%f", &r);
+    
 
+    while (!feof(fin))   //before end of file(file end of file)
+    {
+        r = 0.0 ;
+    
+    fscanf(fin ,"%f", &r);
+    if (r != 0){
     a = 3.14 * r * r ;
 
     printf ("The area is %0.2f \n \n",a);   //0.2 mean 2 position after "."
-    fprintf (fout1 , "The area is %f\n" , r );
+    
     fprintf (fout2 , "The area is %f \n", a );
-    if (fout1 != NULL){
+    }
+    }
+    if (fin != NULL){
 
-        fclose(fout1);
+        fclose(fin);
     }
     if (fout2 != NULL){
 
