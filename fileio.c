027b50aa5ed33845a22dd328181de87f210657af
Than Zaw Toe <microempire37@gmail.com>
2022-09-30 20:52:14 +0630

    2 file opening test and system command

--- fileio.c
+++ fileio.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>     //to use system function command "standard library file"
+
 void area();
+
 int main(){
 
+    for (int i = 0 ; i < 5 ; i++){
+    
     area();
+    system ("pause");
+    
+    }
 
-    return 0;
+    return 0; //
 }
 
 
 void area(){
 
-    FILE* fout = NULL ;
+    FILE* fout1 = NULL ;
+    FILE* fout2 = NULL ;
 
-    fout = fopen("fout.txt" , "w");
+    fout1 = fopen("r.txt" , "a");  
+    fout2 = fopen("a.txt" , "a");   // a = append , w = overwrite , r = read , r+ =read&wirte, w+,a+
 
-    if (fout == NULL )
+    if ((fout1 == NULL) || (fout2 == NULL))     // "&& " = and operator , "||" = or operator
     {
-        printf("fout.txt cannot open");
+        printf("r.txt cannot open");
+    
 
-        return 1 ;
+        return 1; //to show error use return 1
 
     }
 
@@ -30,12 +41,16 @@ void area(){
 
     a = 3.14 * r * r ;
 
-    printf ("The area is %f \n \n",a);
-    fprintf (fout , "The area is %f", a );
+    printf ("The area is %0.2f \n \n",a);   //0.2 mean 2 position after "."
+    fprintf (fout1 , "The area is %f\n" , r );
+    fprintf (fout2 , "The area is %f \n", a );
+    if (fout1 != NULL){
 
-    if (fout != NULL){
+        fclose(fout1);
+    }
+    if (fout2 != NULL){
 
-        fclose(fout);
+        fclose(fout2);
     }
 
 }
